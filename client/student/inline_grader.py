bb5a23f9770beeeb8a901237dd0dd8497ba6dee6
Nitin Pant <nitinpant2006@gmail.com>
2026-06-15 12:28:01 +0530

    Refactor import for final_score in inline_grader.py

--- client/student/inline_grader.py
+++ client/student/inline_grader.py
@@ -1,7 +1,11 @@
-from ShortAnswerGrader.model.final_score import Score
+import pathlib
 
+parent_dir = pathlib.Path(__file__).parent.parent
+        file_path = parent_dir / 'model'
+
+from file_path import final_score
 
 def get_result_inline(qid, answer):
     """Return grading results without requiring an HTTP API call."""
-    fs = Score()
+    fs = final_score.Score()
     return fs.get_final_score(qid, answer)
