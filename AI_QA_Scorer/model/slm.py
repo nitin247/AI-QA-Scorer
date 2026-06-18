03389a0a0675ee4c9a928c2ff6e447774ea1b43b
Nitin Pant <nitinpant2006@gmail.com>
2026-06-18 11:42:36 +0530

    Update slm.py

--- AI_QA_Scorer/model/slm.py
+++ AI_QA_Scorer/model/slm.py
@@ -97,7 +97,7 @@ class Grade:
         data = response.json()
         
         # return output['choices'][0]['message']['content']
-        return data
+        return data['result']
 
 # Run
 # example = Grade()       
